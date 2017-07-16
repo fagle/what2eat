470864c0ee7265d15cf5d0cee2ae992b36f3cc3e
frost <ifagle@qq.com>
2017-07-16 18:35:37 +0800

    struct change

--- src/main/java/FtpBusiness.java
+++
@@ -1,8 +0,0 @@
-
-/*    public static void main(String[] args) throws Exception{
-        test t = new test();
-        t.connect("", "abc.com", 22, "user", "1234");
-        File file = new File("D:/allm3/server/upload/out/artifacts/upload_war_exploded/image/20160325184335902.jpg");
-        t.upload(file);
-    }
-*/
